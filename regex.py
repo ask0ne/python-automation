28fa8ad78376820b07676773a288353474eedb4d
ask0ne <kawade.ak@gmail.com>
2020-09-03 15:43:22 +0530

    bug fixes

--- regex.py
+++ regex.py
@@ -4,10 +4,11 @@ Regular Expressions for validation and matching
 import re
 
 
-class regex:
+class RegexClass:
     '''Class'''
     def __init__(self):
-        self.enter_input()
+        input_text = self.enter_input()
+        self.find_patterns(input_text)
 
     def is_phone(self, example):
         '''Check if given string contains phone number'''
@@ -17,13 +18,17 @@ class regex:
 
     def is_email(self, example):
         '''Email'''
-        email_regex = re.compile(r"( [a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+(\.[a-zA-Z]{2,4}) )")
+        email_regex = re.compile(r''' [a-zA-Z0-9.+]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+ ''', re.VERBOSE)
         return email_regex.findall(example)
 
 
     def enter_input(self):
         '''Input'''
         example = input("Enter text ::")
+        return example
+
+    def find_patterns(self, example):
+        '''Find patterns in string'''
         phone_no = self.is_phone(example)
         email_address = self.is_email(example)
         print('Phone Number Found ::', phone_no)
@@ -31,4 +36,4 @@ class regex:
 
 
 
-regex()
+RegexClass()
